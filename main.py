71355c513e73c98cb0faf6751cc08af58a331a5c
Peter Stridsberg <petta@fightstone.com>
2025-05-06 19:37:06 +0200

    Test updates

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ async def update_items(filename, request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
